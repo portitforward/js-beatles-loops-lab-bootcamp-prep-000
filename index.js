b6505d85f582b2369f986188f849fa7dce460545
Cody Fussell <cody.fussell@gmail.com>
2019-07-14 21:23:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var tempArray = []
+  for (var i=0;i<musicians.length; i++){
+    tempArray.push(musicians[i] + " plays " + instruments[i])
+  }
+  return tempArray
+}
+
+var i = 0;
+function johnLennonFacts (facts) {
+  while (i < facts.length){
+    facts[i] += "!!!"
+    i++
+  }
+  return facts
+}
